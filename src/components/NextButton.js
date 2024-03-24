befb007f5bbda0e7b36d8f05d08d6ff9961b67d9
Bhupender2 <b.mehtaa2021@gmail.com>
2024-03-24 19:46:18 +0530

    .

--- src/components/NextButton.js
+++ src/components/NextButton.js
@@ -2,3 +2,4 @@ export default function NextButton({ dispatch, answer }) {
   if (answer === null) return; // so if answer= null then it will not show the next button basically
   return <button className="btn btn-ui" onClick={()=>dispatch({type:"nextQuestion"})}>Next</button>;
 }
+ 
