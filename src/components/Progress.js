cdc9b15d4b5d5e7ee704276d13b2428e01819e42
Bhupender2 <b.mehtaa2021@gmail.com>
2024-03-24 21:22:20 +0530

    progress bar logic added in comment

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -7,7 +7,9 @@ export default function Progress({
 }) {
   return (
     <header className="progress">
-      <progress max={numQuestions} value={index+Number(answer!==null)} />
+      <progress max={numQuestions} value={index + Number(answer !== null)} />
+
+      {/* this is one of the trickiest thing ..value attribute is increasing on clicking the option (which will store in answer variable) but jab hum click karenge next button pe toh index ki value +1 se increase ho jaaegi or answer phirse null ho jaaega ..so isliye progress bar same rahega coz ek baar answer ka +1 add hora h ..but next button pe click karte time answer null hojaara h but ussi time pe index ka +1 add hoga isliye progress bar same rahega  */}
       <p>
         Question <strong>{index + 1}</strong>/{numQuestions}
       </p>
