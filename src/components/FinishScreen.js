cfd53346b2d6949a43294714bebe107fd8e436dd
Bhupender2 <b.mehtaa2021@gmail.com>
2024-03-25 14:43:34 +0530

    percentage and emoji added in FinishScreen component

--- src/components/FinishScreen.js
+++ src/components/FinishScreen.js
@@ -1,8 +1,19 @@
 export default function FinishScreen({ points, totalPoints }) {
   const percentage = (points / totalPoints) * 100;
+
+  let emoji;
+  if (percentage === 100) emoji = "🎖️";
+  if (percentage < 100 && percentage >= 80) emoji = "🎉";
+  if (percentage < 80 && percentage >= 50) emoji = "🤔";
+  if (percentage < 50 && percentage > 0) emoji = "😢";
+  if (percentage === 0) emoji = "🤦";
   return (
-    <p className="result">
-      You have scored <strong>{points}</strong> out of  {totalPoints} {Math.ceil(percentage)} %
-    </p>
+    <>
+      <p className="result">
+        <span>{emoji}</span> You have scored <strong>{points}</strong> out of{" "}
+        {totalPoints} {Math.ceil(percentage)} %
+      </p>
+      <p className="highscore"></p>
+    </>
   );
 }
